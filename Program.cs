74b7e1c65360d39e61fc841d63e99835823b8616
Christian Moyes <moyechri846@gmail.com>
2023-09-18 11:01:43 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Method_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number for a joke");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Joke(num);
+        }
+
+        public static void Joke()
+        {
+            Console.WriteLine("99 little bugs left in the code");
+            Thread.Sleep(1000);
+            Console.WriteLine("99 little bugs left");
+            Thread.Sleep(1000);
+            Console.WriteLine("Fix a bug, run it again");
+            Thread.Sleep(1000);
+            Console.WriteLine("100 little bugs left in the code");
+        }
+
+        public static void Joke(int numBugs)
+        {
+            Console.WriteLine($"{numBugs} little bugs left in the code");
+            Thread.Sleep(1000);
+            Console.WriteLine($"{numBugs} little bugs left");
+            Thread.Sleep(1000);
+            Console.WriteLine($"Fix a bug, run it again");
+            Thread.Sleep(1000);
+            Console.WriteLine($"{++numBugs} little bugs left in the code");
+        }
+
+    }
+}
